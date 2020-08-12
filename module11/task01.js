9c91d24e0af576a19d6e744db306204765b81674
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-08-12 15:19:12 +0300

    сделал первое задание

---
+++ module11/task01.js
@@ -0,0 +1,30 @@
+const refs = {
+  btnStart: document.querySelector("button[data-action='start'"),
+  btnStop: document.querySelector('button[data-action="stop"'),
+};
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const setRandomColor = () => {
+  const newColor = colors[randomIntegerFromInterval(colors.length - 1)];
+  console.log(newColor);
+  document.body.style.backgroundColor = newColor;
+};
+
+let interval = undefined;
+
+refs.btnStart.addEventListener(
+  "click",
+  (e) =>
+    (interval = interval ? interval : setInterval(() => setRandomColor(), 1000))
+);
+refs.btnStop.addEventListener("click", (e) => clearInterval(interval));
